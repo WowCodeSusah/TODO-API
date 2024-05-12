fd81bea415e7fb02e58a471d9125b54dfcc0a3ec
Michael Bengawan <michaelbengawan1@gmail.com>
2024-05-12 23:37:21 +0700

    second commmit

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from random import randint
 from . import models
 from .database import engine, get_db
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+
+# Users
 
 class User(BaseModel):
     name: str
@@ -20,6 +23,20 @@ class UpdateUser(BaseModel):
 
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create a new ID
 def getNewID(baseUser):
@@ -57,7 +74,7 @@ def get_all_users(db: Session = Depends(get_db)):
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if user == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id: {id} was not found")
     return {"user": user}
 
 @app.post("/users", status_code=status.HTTP_201_CREATED)
@@ -77,7 +94,7 @@ def add_user(data: User, db: Session = Depends(get_db)):
 def delete_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id)
     if user == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"user with id: {id} was not found")
     user.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
@@ -87,8 +104,57 @@ def update_user(id: int, user: UpdateUser,  db: Session = Depends(get_db)):
     getUser = db.query(models.User).filter(models.User.id == id)
     selectedUser = getUser.first()
     if selectedUser == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"user with id: {id} was not found")
     getUser.update(user.model_dump(), synchronize_session=False)
 
     db.commit()
     return {"Updated User" : getUser.first()}
+
+# To do Items
+
+class Task(BaseModel):
+    title: str
+    description: str
+    checked: bool
+
+@app.get("/tasks")
+def get_all_tasks(db: Session = Depends(get_db)):
+    taskAll = db.query(models.Tasks).all()
+    return {"all_task": taskAll}
+
+@app.get("/tasks/{id}")
+def get_task(id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Tasks.id == id).first()
+    if task == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"task with id: {id} was not found")
+    return {"task": task}
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+def add_task(data: Task, db: Session = Depends(get_db)):
+    taskDict = data.model_dump()
+    newTask = models.Tasks(**taskDict)
+    db.add(newTask)
+    db.commit()
+    db.refresh(newTask)
+
+    return {"Task has been added" : newTask}
+
+@app.delete("/tasks/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Tasks).filter(models.Tasks.id == id)
+    if task == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"task with id: {id} was not found")
+    task.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/tasks/{id}")
+def update_task(id: int, task: Task,  db: Session = Depends(get_db)):
+    getTask = db.query(models.Tasks).filter(models.Tasks.id == id)
+    selectedTask = getTask.first()
+    if selectedTask == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"task with id: {id} was not found")
+    getTask.update(task.model_dump(), synchronize_session=False)
+
+    db.commit()
+    return {"Updated Task" : getTask.first()}
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from sqlalchemy.sql import func
 
 class User(Base):
@@ -12,5 +12,11 @@ class User(Base):
     email = Column(String, nullable=False)
     timeCreated = Column(DateTime(timezone=True), server_default=func.now())
     
-    
+class Tasks(Base):
+    __tablename__ = "Tasks"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    checked = Column(Boolean, nullable=False)
+    timeCreated = Column(DateTime(timezone=True), server_default=func.now())
     
